77bac43ef69fc10a06bda56f6758de01333b4d42
zxc85 <xian_chee@hotmail.com>
2025-02-22 19:58:35 +0800

    Fix bug when user info being cache

--- userService/src/main/java/com/ecommerce/user/repository/UserRepository.java
+++ userService/src/main/java/com/ecommerce/user/repository/UserRepository.java
@@ -27,11 +27,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findOneByLogin(String login);
 
     @EntityGraph(attributePaths = "authorities")
-    @Cacheable(cacheNames = USERS_BY_LOGIN_CACHE)
     Optional<User> findOneWithAuthoritiesByLogin(String login);
 
     @EntityGraph(attributePaths = "authorities")
-    @Cacheable(cacheNames = USERS_BY_EMAIL_CACHE)
     Optional<User> findOneWithAuthoritiesByEmailIgnoreCase(String email);
 
     Page<User> findAllByIdNotNullAndActivatedIsTrue(Pageable pageable);
